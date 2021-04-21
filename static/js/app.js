59196b342e1b40b0bda3c389bbdd8493814710af
sofi7771 <sofanitinchrist@gmail.com>
2021-04-21 16:56:34 -0500

    added event handlers

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
 console.log ("app.js loaded");
+function DrawBargraph(sampleId){
+    console.log('DrawBargraph($({sampleId})');
+}
+function DrawBargraph(sampleId){
+    console.log('DrawBargraph($({sampleId})');
+}
+function ShowMetadata(sampleId){
+    console.log('ShowMetadata($({sampleId})');
+}
+
+function optionchanged(newSampleId){
+    console.log('User selected $(newSampleId');
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+
+}
+
 
 function InitDashboard() {
     console.log("InitDashboard()"); 
@@ -8,6 +27,26 @@ function InitDashboard() {
       
       d3.json("data/samples.json").then(function(data){
           console.log(data);
+
+        var sampleNames = data.names;
+        sampleNames.forEach(sampleId => {
+             selector.append("option")
+                   .text(sampleId)
+                   .property("value", sampleId);
+        });
+
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+        DrawBubblechart(id);
+        ShowMetadata(id);
+
+
+
+
+
+
+
       });
 
       
